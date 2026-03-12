eab63c1d771ed3fd69cb9eb439743ed6fb81013b
kuankuan <2163826131@qq.com>
2026-03-12 21:56:13 +0800

    使用统一抽离的@kuankuan/assist-2026的功能组件

--- src/scripts/theme.ts
+++ src/scripts/theme.ts
@@ -1,16 +1,1 @@
-import storageRef from './storageRef';
-
-export const themeValue = storageRef('theme', 'auto', localStorage);
-export const themeValueList = ['auto', 'light', 'dark'];
-const matcher = window.matchMedia('(prefers-color-scheme: dark)');
-
-export const theme = computed(() => {
-  if (themeValue.value === 'auto') {
-    return matcher.matches ? 'dark' : 'light';
-  }
-  return themeValue.value;
-});
-function updateDomTheme() {
-  document.documentElement.dataset.theme = theme.value;
-}
-watch(theme, updateDomTheme, { immediate: true });
+export * from "@kuankuan/assist-2026/src/theme";
